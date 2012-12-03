639939541cb9ceae31b0dfdc71b5f50ba5071d3d
Luca Casartelli <lc.casartelli@gmail.com>
2012-12-03 08:26:22 +0100

    Added "print n primi" example Fix nprimo with n = 2

--- printprimi.asm
+++ printprimi.asm
@@ -1,28 +1,28 @@
-# Calcola n-esimo numero primo LC
+# Calcola n numeri primi LC
 
-# 	$t0 			posizione finale numero primo
-# 	$t1 			posizione temporanea numero primo
-# 	$t2				risultato
+# 	$t0 			numeri di primi da stampare
+# 	$t1 			contatore numeri primi stampati
 # 	$t4				contatore generico (incrementato ad ogni ciclo  di +1)
 # 	$t5 			costante incremento
 # 	$t6 			divisore
 # 	$t7, $t8, $t9	registri di appoggio
 
 
-
 .data
-	input_message: .asciiz "Inserisci l'indice n-esimo numero primo da stampare: "
+	input_message: .asciiz "Inserisci numero di primi da stampare: "
 	error_message: .asciiz "Numero non valido"
-	output_message: .asciiz "Numero primo: "
+	output_message: .asciiz "Numeri primi: "
+	space: .asciiz " "
 
 .text
 .globl main								#rende visibile il metodo main al di fuori del file (vedi doc)
 
 main:
 
-	move $t4, $zero						#contatore = 0
+	move $t4, $zero						
 	li $t5, 1
 	li $t4, 2
+	li $t1, 0
 
 	
 	li $v0, 4							#stampa stringa input numero	
@@ -33,19 +33,21 @@ main:
 	syscall
 	move $t0, $v0						#memorizza numero
 
-	ble $t0, $zero, error_message			#check numero: se n=0 -> messaggio errore
+	ble $t0, $zero, error_message		#check numero: se n=0 -> messaggio errore
 	
-	move $t2, $t4						#numero primo attuale = 2					
-	li $t1,1 							#contatore numeri primi = 1
+	
+	
 
-	beq $t0, $t1, print_result			#caso semplice n = 1 -> risultato = 2
+	li $v0, 4 							#Stampa label risultato
+	la $a0, output_message
+	syscall
 
-
-	#Caso generico n > 1
 
 	while:								#CICLO: TROVA N-ESIMO NUMERO PRIMO
 
-		beq $t1, $t0, print_result		#controlla se il ciclo è finito
+		beq $t1, $t0, end				#controlla se il ciclo è finito
+
+		beq $t1, $zero, primo 			#caso semplice n = 1 -> risultato = 2
 
 		add $t4, $t4, $t5				#contatore generale ++
 		li $t6, 2						#divisore di partenza 2
@@ -70,18 +72,15 @@ main:
 		primo:							#TROVATO NUMERO PRIMO: MEMORIZZA VALORE E INDICE
 
 			add $t1, $t1, $t5			#contatore numero primo ++
-			move $t2, $t4				#risultato = numero primo attuale			
+			li $v0, 1
+			move $a0, $t4
+			syscall
+
+			li $v0, 4					#Stampa spazio
+			la $a0, space
+			syscall
 		
 		j while
-	
-	print_result:						#STAMPA RISULTATO
-		li $v0, 4
-		la $a0, output_message
-		syscall
-		li $v0, 1
-		move $a0, $t2
-		syscall
-		j end
 
 	error:								#STAMPA ERRORE
 		li $v0, 4
